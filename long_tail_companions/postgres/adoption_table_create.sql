eb3efa80fef5647b59a5e35fadd74660ac9d4630
Maggie Hays <maggiem.hays@gmail.com>
2022-06-17 17:56:03 -0500

    include schema details and update path to raw data

--- long_tail_companions/postgres/adoption_table_create.sql
+++ long_tail_companions/postgres/adoption_table_create.sql
@@ -1,6 +1,8 @@
-drop table if exists pets cascade;
+create schema if not exists adoption;
 
-create table pets
+drop table if exists adoption.pets cascade;
+
+create table adoption.pets
 (
 pk bigint primary key not null
 , profile_id varchar
@@ -10,18 +12,18 @@ pk bigint primary key not null
 )
 ;
 
-comment on table pets is 'this table contains records of all pets that have been considered for adoption regardless of outcome';
-comment on column pets.pk is 'primary key of pets table';
-comment on column pets.status is 'current status of pet';
-comment on column pets.profile_id is 'external lookup ID to the pet_profile table';
-comment on column pets.created_at is 'UTC timestamp when record was created';
-comment on column pets.updated_at is 'UTC timestamp when record was last updated';
+comment on table adoption.pets is 'this table contains records of all adoption.pets that have been considered for adoption regardless of outcome';
+comment on column adoption.pets.pk is 'primary key of adoption.pets table';
+comment on column adoption.pets.status is 'current status of pet';
+comment on column adoption.pets.profile_id is 'external lookup ID to the pet_profile table';
+comment on column adoption.pets.created_at is 'UTC timestamp when record was created';
+comment on column adoption.pets.updated_at is 'UTC timestamp when record was last updated';
 
-\copy pets from 'adoption/postgres/sample_data/pets.csv' delimiter ',' csv header;
+\copy adoption.pets from 'data/pets.csv' delimiter ',' csv header;
 
-drop table if exists humans cascade;
+drop table if exists adoption.humans cascade;
 
-create table humans
+create table adoption.humans
 (
 pk bigint primary key not null
 , profile_id varchar
@@ -31,19 +33,19 @@ pk bigint primary key not null
 )
 ;
 
-comment on table humans is 'this table contains records of all humans that have ever pursued an adoption';
+comment on table adoption.humans is 'this table contains records of all adoption.humans that have ever pursued an adoption';
 
-comment on column humans.pk is 'primary key of humans table';
-comment on column humans.profile_id is 'external lookup ID to the human_profile table';
-comment on column humans.email is 'email address currently associated with this human';
-comment on column humans.created_at is 'UTC timestamp when record was created';
-comment on column humans.updated_at is 'UTC timestamp when record was last updated';
+comment on column adoption.humans.pk is 'primary key of adoption.humans table';
+comment on column adoption.humans.profile_id is 'external lookup ID to the human_profile table';
+comment on column adoption.humans.email is 'email address currently associated with this human';
+comment on column adoption.humans.created_at is 'UTC timestamp when record was created';
+comment on column adoption.humans.updated_at is 'UTC timestamp when record was last updated';
 
-\copy humans from 'adoption/postgres/sample_data/humans.csv' delimiter ',' csv header;
+\copy adoption.humans from 'data/humans.csv' delimiter ',' csv header;
 
-drop table if exists adoptions cascade;
+drop table if exists adoption.adoptions cascade;
 
-create table adoptions 
+create table adoption.adoptions 
 (
 pk bigint primary key not null
 , pet_fk  bigint not null
@@ -54,18 +56,18 @@ pk bigint primary key not null
 )
 ;
 
-comment on table adoptions is 'this table contains records of all attempted adoptions regardless of outcome';
+comment on table adoption.adoptions is 'this table contains records of all attempted adoption.adoptions regardless of outcome';
 
-comment on column adoptions.pk is 'primary key of adoptions table';
-comment on column adoptions.pet_fk is 'foreign key of the pets table';
-comment on column adoptions.human_fk is 'foreign key of the humans table';
-comment on column adoptions.status is 'latest status of adoption';
-comment on column adoptions.created_at is 'UTC timestamp when record was created';
-comment on column adoptions.updated_at is 'UTC timestamp when record was last updated';
+comment on column adoption.adoptions.pk is 'primary key of adoption.adoptions table';
+comment on column adoption.adoptions.pet_fk is 'foreign key of the adoption.pets table';
+comment on column adoption.adoptions.human_fk is 'foreign key of the adoption.humans table';
+comment on column adoption.adoptions.status is 'latest status of adoption';
+comment on column adoption.adoptions.created_at is 'UTC timestamp when record was created';
+comment on column adoption.adoptions.updated_at is 'UTC timestamp when record was last updated';
 
 -- add in pk/fk constraints
 
-alter table adoptions add foreign key (pet_fk) references pets(pk);
-alter table adoptions add foreign key (human_fk) references humans(pk);
+alter table adoption.adoptions add foreign key (pet_fk) references adoption.pets(pk);
+alter table adoption.adoptions add foreign key (human_fk) references adoption.humans(pk);
 
-\copy adoptions from 'adoption/postgres/sample_data/adoptions.csv' delimiter ',' csv header;
+\copy adoption.adoptions from 'data/adoptions.csv' delimiter ',' csv header;
--- long_tail_companions/postgres/ecommerce_table_create.sql
+++ long_tail_companions/postgres/ecommerce_table_create.sql
@@ -1,8 +1,8 @@
 create schema if not exists ecommerce;
 
-drop table if exists users cascade;
+drop table if exists ecommerce.users cascade;
 
-create table users
+create table ecommerce.users
 (
 pk bigint primary key not null
 , email varchar
@@ -11,18 +11,18 @@ pk bigint primary key not null
 )
 ;
 
-comment on table users is 'this table contains records of all users that have ever made a purchase on the ecommerce site';
+comment on table ecommerce.users is 'this table contains records of all users that have ever made a purchase on the ecommerce site';
 
-comment on column users.pk is 'primary key of users table' ;
-comment on column users.email is 'email address currently associated with the user';
-comment on column users.created_at is 'UTC timestamp when record was created';
-comment on column users.updated_at is 'UTC timestamp when record was last updated';
+comment on column ecommerce.users.pk is 'primary key of users table' ;
+comment on column ecommerce.users.email is 'email address currently associated with the user';
+comment on column ecommerce.users.created_at is 'UTC timestamp when record was created';
+comment on column ecommerce.users.updated_at is 'UTC timestamp when record was last updated';
 
-\copy users from './ecommerce/postgres/sample_data/users.csv' delimiter ',' csv header;
+\copy ecommerce.users from 'data/users.csv' delimiter ',' csv header;
 
-drop table if exists items cascade;
+drop table if exists ecommerce.items cascade;
 
-create table items 
+create table ecommerce.items 
 (
 pk bigint primary key not null
 , name varchar not null
@@ -33,21 +33,21 @@ pk bigint primary key not null
 )
 ;
 
-comment on table items is 'this table contains a record for each item that is for sale in the LTC online store';
+comment on table ecommerce.items is 'this table contains a record for each item that is for sale in the LTC online store';
 
-comment on column items.pk is 'primary key of items table';
-comment on column items.name is 'common name of item for sale';
-comment on column items.type is 'type of item for sale';
-comment on column items.price is 'price of item in USD';
-comment on column items.created_at is 'UTC timestamp when record was created';
-comment on column items.updated_at is 'UTC timestamp when record was last updated';
+comment on column ecommerce.items.pk is 'primary key of items table';
+comment on column ecommerce.items.name is 'common name of item for sale';
+comment on column ecommerce.items.type is 'type of item for sale';
+comment on column ecommerce.items.price is 'price of item in USD';
+comment on column ecommerce.items.created_at is 'UTC timestamp when record was created';
+comment on column ecommerce.items.updated_at is 'UTC timestamp when record was last updated';
 
-\copy items from 'ecommerce/postgres/sample_data/items.csv' delimiter ',' csv header;
+\copy ecommerce.items from 'data/items.csv' delimiter ',' csv header;
 
 
-drop table if exists purchases cascade;
+drop table if exists ecommerce.purchases cascade;
 
-create table purchases
+create table ecommerce.purchases
 (
 pk bigint primary key not null
 , user_fk bigint not null
@@ -60,22 +60,22 @@ pk bigint primary key not null
 )
 ;
 
-comment on table purchases is 'this table contains a record for each purchase at the LTC online store';
+comment on table ecommerce.purchases is 'this table contains a record for each purchase at the LTC online store';
 
-comment on column purchases.pk is 'primary key of purchases table';
-comment on column purchases.user_fk is 'foreign key of the users table';
-comment on column purchases.status is 'latest status of the purchase';
-comment on column purchases.purchase_amount is 'total purchase amount in USD, excluding taxes';
-comment on column purchases.tax_amount is 'total tax amount in USD';
-comment on column purchases.total_amount is 'total transaction amount in USD';
-comment on column purchases.created_at is 'UTC timestamp when record was created';
-comment on column purchases.updated_at is 'UTC timestamp when record was last updated';
+comment on column ecommerce.purchases.pk is 'primary key of purchases table';
+comment on column ecommerce.purchases.user_fk is 'foreign key of the users table';
+comment on column ecommerce.purchases.status is 'latest status of the purchase';
+comment on column ecommerce.purchases.purchase_amount is 'total purchase amount in USD, excluding taxes';
+comment on column ecommerce.purchases.tax_amount is 'total tax amount in USD';
+comment on column ecommerce.purchases.total_amount is 'total transaction amount in USD';
+comment on column ecommerce.purchases.created_at is 'UTC timestamp when record was created';
+comment on column ecommerce.purchases.updated_at is 'UTC timestamp when record was last updated';
 
-\copy purchases from 'ecommerce/postgres/sample_data/purchases.csv' delimiter ',' csv header;
+\copy ecommerce.purchases from 'data/purchases.csv' delimiter ',' csv header;
 
-drop table if exists purchase_items cascade;
+drop table if exists ecommerce.purchase_items cascade;
 
-create table purchase_items
+create table ecommerce.purchase_items
 (
 pk bigint primary key not null
 , purchase_fk bigint not null
@@ -84,17 +84,17 @@ pk bigint primary key not null
 )
 ;
 
-comment on table purchase_items is 'this table contains a record for each item included in a purchase from the LTC online store';
+comment on table ecommerce.purchase_items is 'this table contains a record for each item included in a purchase from the LTC online store';
 
-comment on column purchase_items.pk is 'primary key of purchase items table';
-comment on column purchase_items.purchase_fk is 'foreign key of the purchaeses table';
-comment on column purchase_items.item_fk is 'foreign key of the items table';
-comment on column purchase_items.quantity is 'number of items purchased';
+comment on column ecommerce.purchase_items.pk is 'primary key of purchase items table';
+comment on column ecommerce.purchase_items.purchase_fk is 'foreign key of the purchaeses table';
+comment on column ecommerce.purchase_items.item_fk is 'foreign key of the items table';
+comment on column ecommerce.purchase_items.quantity is 'number of items purchased';
 
-\copy purchase_items from 'ecommerce/postgres/sample_data/purchase_items.csv' delimiter ',' csv header;
+\copy ecommerce.purchase_items from 'data/purchase_items.csv' delimiter ',' csv header;
 
 -- add in pk/fk constraints
 
-alter table purchases add foreign key (user_fk) references users(pk);
-alter table purchase_items add foreign key (purchase_fk) references purchases(pk);
-alter table purchase_items add foreign key (item_fk) references items(pk);
+alter table ecommerce.purchases add foreign key (user_fk) references ecommerce.users(pk);
+alter table ecommerce.purchase_items add foreign key (purchase_fk) references ecommerce.purchases(pk);
+alter table ecommerce.purchase_items add foreign key (item_fk) references ecommerce.items(pk);
